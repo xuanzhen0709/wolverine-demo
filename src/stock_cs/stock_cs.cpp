b842a89811be72b90ed18eef06d2056fd1d8db9b
nickchenyj90@126.com <nickchenyj90@126.com>
2023-04-21 09:35:50 +0800

    update stock_cs demo

---
+++ src/stock_cs/stock_cs.cpp
@@ -0,0 +1,154 @@
+#include <limits>
+#include <wolverine/common.hpp>
+#include <wolverine/config.hpp>
+#include <wolverine/event.hpp>
+#include <wolverine/fmt/core.h>
+#include <wolverine/marketdata.hpp>
+#include <wolverine/signal.hpp>
+
+#include <array>
+#include <cmath>
+#include <string_view>
+
+using namespace cfi::wolverine;
+
+namespace nickchenyj {
+namespace stock_cs {
+
+class Signal {
+public:
+  Signal();
+
+  void initialize(const Config *root);
+  void set_apis(SignalApis);
+  void on_sod(uint32_t date, const SodEvent *ev);
+  void on_snapshot(const SnapshotEvent *ev);
+  void on_bar(const BarEvent *ev);
+  void on_cs_snapshot(const CsSnapshotEvent *ev);
+  void on_eod(uint32_t date);
+
+private:
+  SignalApis m_apis = {nullptr};
+};
+
+// function definitions
+
+Signal::Signal() { on_sod(0, nullptr); }
+
+void Signal::initialize(const Config *root) {
+  // subscribe data
+  {
+    const auto &md_cfg = root->getChildNode("marketdata");
+    const size_t md_nr = md_cfg.getSize();
+    for (size_t md_idx = 0; md_idx < md_nr; ++md_idx) {
+      const auto &this_cfg = md_cfg.getChildNodeByIdx(md_idx);
+      const auto type = this_cfg.getValue<std::string>("type");
+      const auto symbols_cfg = this_cfg.getChildNode("symbols");
+      std::vector<std::string> symbols;
+      for (size_t sym_idx = 0; sym_idx < symbols_cfg.getSize(); ++sym_idx) {
+        symbols.push_back(symbols_cfg.getChildElement<std::string>(sym_idx));
+      }
+      m_apis.subscribe(m_apis.token, type, symbols);
+    }
+  }
+}
+
+void Signal::set_apis(SignalApis apis) { m_apis = apis; }
+
+void Signal::on_sod(uint32_t date, const SodEvent *ev) {
+  if (!ev) {
+    return;
+  }
+  // for now in cross-sectional mode, we get the full list of stock names on
+  // start of each day call set_targets() everyday
+  std::vector<std::string> targets;
+  fmt::print("Signal on_sod ins_nr={},", ev->ins_nr);
+  for (decltype(ev->ins_nr) i = 0; i < ev->ins_nr; ++i) {
+    const auto *ms = ev->ms[i];
+    if (i) {
+      fmt::print(";{}", ms->instrument);
+    } else {
+      fmt::print("{}", ms->instrument);
+    }
+    std::string ins{ms->instrument};
+    ins.erase(std::find(ins.begin(), ins.end(), '\0'), ins.end());
+
+    std::string exch{ms->exchange};
+    exch.erase(std::find(exch.begin(), exch.end(), '\0'), exch.end());
+
+    std::string symbol = ins + "." + exch;
+    targets.emplace_back(symbol);
+  }
+  fmt::print("\n");
+  // set trading targets
+  m_apis.set_targets(m_apis.token, targets);
+}
+
+void Signal::on_snapshot(const SnapshotEvent *ev) {
+  // market data update
+  fmt::print("on_snapshot,{},{},{}\n", ev->snapshot->exchtime,
+             ev->snapshot->localtime, ev->ms->instrument);
+}
+
+void Signal::on_bar(const BarEvent *ev) { fmt::print("on_bar\n"); }
+
+void Signal::on_cs_snapshot(const CsSnapshotEvent *ev) {
+  fmt::print("on_cs_snapshot,exchtime:{},ins_nr:{},fld_nr:{}\n", ev->exchtime,
+             ev->ins_nr, ev->fld_nr);
+}
+
+void Signal::on_eod(uint32_t date) { fmt::print("on_eod\n"); }
+
+} // namespace stock_cs
+} // namespace nickchenyj
+
+using namespace nickchenyj::stock_cs;
+
+C_DECLARATION_BEGIN;
+
+static SignalOps my_ops = {
+    .initialize = [](void *hdl, const cfi::wolverine::Config *root) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->initialize(root);
+    },
+
+    .set_apis = [](void *hdl, cfi::wolverine::SignalApis apis) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->set_apis(apis);
+    },
+
+    .on_sod = [](void *hdl, uint32_t date,
+                 const cfi::wolverine::SodEvent *ev) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->on_sod(date, ev);
+    },
+
+    .on_snapshot = [](void *hdl,
+                      const cfi::wolverine::SnapshotEvent *ev) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->on_snapshot(ev);
+    },
+
+    .on_bar = [](void *hdl, const cfi::wolverine::BarEvent *ev) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->on_bar(ev);
+    },
+
+    .on_cs_snapshot = [](void *hdl,
+                         const cfi::wolverine::CsSnapshotEvent *ev) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->on_cs_snapshot(ev);
+    },
+
+    .on_eod = [](void *hdl, uint32_t date) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->on_eod(date);
+    },
+};
+
+void on_create(void **ptr, SignalOps *ops) {
+  *ptr = new Signal{};
+  *ops = my_ops;
+}
+
+C_DECLARATION_END;
