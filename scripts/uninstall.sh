ef5567c74b6e8036e770ff21523367f6297b8bd6
nickchenyj90@126.com <nickchenyj90@126.com>
2023-06-17 16:50:42 +0800

    add uninstall.sh

---
+++ scripts/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -v ~/.local/bin/wl-* ~/.local/bin/wlmd*
+
+rm -v ~/.local/lib/wlmd-* ~/.local/lib/wlsigsaver-*
+rm -v ~/.local/lib/libwlutils.a
+rm -v ~/.local/lib/wlsig-*
+
+rm -v -rf ~/.local/include/wolverine
