2167e67f7ac72c3ec2ca265d60af84165a7eed58
nickchenyj90@126.com <nickchenyj90@126.com>
2023-07-17 13:36:19 +0800

    add snapshot demo

---
+++ src/snapshot/main.cpp
@@ -0,0 +1,151 @@
+#include <wolverine/common.hpp>
+#include <wolverine/config.hpp>
+#include <wolverine/event.hpp>
+#include <wolverine/fmt/core.h>
+#include <wolverine/logging.hpp>
+#include <wolverine/marketdata.hpp>
+#include <wolverine/signal.hpp>
+#include <wolverine/utils/time.hpp>
+
+#include <array>
+#include <cmath>
+#include <filesystem>
+#include <string_view>
+
+using namespace cfi::wolverine;
+
+namespace nickchenyj {
+namespace csstock {
+
+class Signal {
+public:
+  Signal();
+
+  void initialize(const Config *root);
+  void set_apis(SignalApis);
+  void load_state(const std::string &indir);
+  void save_state(const std::string &outdir);
+  void on_sod(uint32_t date, const SodEvent *ev);
+  void on_eod(uint32_t date);
+
+  void on_snapshot(const SnapshotEvent *ev);
+
+private:
+  SignalApis m_apis = {nullptr};
+  size_t m_cnt = 0;
+};
+
+// function definitions
+
+Signal::Signal() { on_sod(0, nullptr); }
+
+void Signal::initialize(const Config *root) {}
+
+void Signal::set_apis(SignalApis apis) { m_apis = apis; }
+
+void Signal::load_state(const std::string &indir) {
+  LOG_INFO("loading state from dir {}\n", indir);
+}
+
+void Signal::save_state(const std::string &outdir) {
+  LOG_INFO("saving state to dir {}\n", outdir);
+}
+
+void Signal::on_sod(uint32_t date, const SodEvent *ev) {
+  m_cnt = 0;
+  if (!ev) {
+    return;
+  }
+  // NOTE:
+  // for now in cross-sectional mode, we get the full list of stock names
+  // on start of each day
+  LOG_INFO("ins_nr={}\n", ev->ins_nr);
+  for (decltype(ev->ins_nr) i = 0; i < ev->ins_nr; ++i) {
+    const auto *ms = ev->ms[i];
+    std::string ins{ms->instrument};
+    ins.erase(std::find(ins.begin(), ins.end(), '\0'), ins.end());
+
+    std::string exch{ms->exchange};
+    exch.erase(std::find(exch.begin(), exch.end(), '\0'), exch.end());
+
+    std::string symbol = ins + "." + exch;
+  }
+}
+
+void Signal::on_eod(uint32_t date) { LOG_INFO("{} updates received\n", m_cnt); }
+
+void Signal::on_snapshot(const SnapshotEvent *ev) {
+  LOG_DEBUG("exchtime:{}/{},localtime:{}/{}\n", ev->snapshot->exchtime,
+            time::exchtime_to_str(ev->snapshot->exchtime),
+            ev->snapshot->localtime,
+            time::epoch_to_str(ev->snapshot->localtime));
+  // to access level-based fields
+  // const TYPE* arr = ev->flds[fld].xxx_ptrs[lvl];
+  // where arr is a pointer to an array of length ins_nr
+
+  // to access non-level-based fields
+  // const TYPE* arr = ev->flds[fld].xxx_ptr;
+  // where arr is a pointer to an array of length ins_nr
+
+  // for (int i = 0; i < static_cast<int>(CsSnapshotEvent::FldType::_MAX); ++i)
+  // {
+  //   const auto &fld = ev->flds[i];
+  //   LOG_INFO_CONT("{},{}\n", i, fmt::ptr(fld.void_ptr));
+  // }
+  ++m_cnt;
+  // we use m_cnt as the sig value for each target
+  const double sig_val = m_cnt;
+  m_apis.update_signal(m_apis.token, ev->snapshot->exchtime,
+                       ev->snapshot->localtime, 1, &sig_val);
+}
+
+} // namespace csstock
+} // namespace nickchenyj
+
+using nickchenyj::csstock::Signal;
+
+C_DECLARATION_BEGIN;
+
+static const SignalOps my_ops = {
+    .initialize = [](void *hdl, const Config *root) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->initialize(root);
+    },
+
+    .set_apis = [](void *hdl, SignalApis apis) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->set_apis(apis);
+    },
+
+    .load_state = [](void *hdl, const std::string &indir) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->load_state(indir);
+    },
+
+    .save_state = [](void *hdl, const std::string &outdir) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->save_state(outdir);
+    },
+
+    .on_sod = [](void *hdl, uint32_t date, const SodEvent *ev) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->on_sod(date, ev);
+    },
+
+    .on_eod = [](void *hdl, uint32_t date) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->on_eod(date);
+    },
+
+    .on_snapshot = [](void *hdl, const SnapshotEvent *ev) -> void {
+      auto *ptr = reinterpret_cast<Signal *>(hdl);
+      ptr->on_snapshot(ev);
+    },
+};
+
+void on_create(void **ptr, SignalOps *ops) {
+  *ptr = new Signal{};
+  *ops = my_ops;
+}
+
+C_DECLARATION_END;
