19a70a9b17dce4f2ca928f65aafdf87e351fe1fe
nickchenyj90@126.com <nickchenyj90@126.com>
2023-07-20 00:36:40 +0800

    demo: update cmake template for c++ signals

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -3,7 +3,7 @@
 rm -v ~/.local/bin/wl-* ~/.local/bin/wlmd*
 
 rm -v ~/.local/lib/wlmd-* ~/.local/lib/wlsigsaver-*
-rm -v ~/.local/lib/libwlutils.a
+rm -v ~/.local/lib/libwl-*
 rm -v ~/.local/lib/wlsig-*
 
 rm -v -rf ~/.local/include/wolverine
