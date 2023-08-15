f5a140a6265089409b61bb2588e9602829cd54d0
孙 晴 <sunqing@centuryfrontier.com>
2023-08-15 06:16:13 +0000

    initial support for continuous ic calculator

--- src/ic_calculator/main.pyx
+++ src/ic_calculator/main.pyx
@@ -4,7 +4,7 @@ import yaml
 from pathlib import Path
 import pandas as pd
 from typing import Dict, Set
-
+from abc import ABC, abstractmethod
 from cfi.wolverine.signal import *
 
 import cython
@@ -14,28 +14,96 @@ from libc.stdint cimport *
 # speed up numpy arrays
 cimport numpy as np
 np.import_array()
+np.set_printoptions(suppress=True)
+import time
 
+####################################################################################################
+import datetime
+import numpy as np
+from pathlib import Path
+from typing import List, Set, Union
+
+class Calendar:
+    """
+    This class provides trading calendar lookups.
+
+    A typical use case would be:
+
+    Calendar.get_instance().next_business_day(20211202, 1)
+    """
+    __INSTANCE = None
+
+    def __init__(self):
+        path: Path = Path("/mnt/nas-3/CTA/Data/ChinaTradingDates.txt")
+        with open(path) as fin:
+            lines = [int(x) for x in fin.read().splitlines()]
+            self.dates: np.ndarray = np.array(lines, dtype=np.uint32)
+            self.date_set: Set[int] = set(lines)
+
+    @staticmethod
+    def get_instance() -> "Calendar":
+        """
+        Return:
+            the global Calendar instance
+        """
+        if Calendar.__INSTANCE is None:
+            Calendar.__INSTANCE = Calendar()
+        return Calendar.__INSTANCE
+
+    def next_business_day(self, date: Union[str, int], shift: int = 1) -> int:
+        """
+        find the business day with a an optional shift
+
+        Args:
+            date: reference date
+            shift: offset to the reference date
+
+        Return:
+            date
+        """
+        date_int = int(date)
+        idx: int = int(np.searchsorted(self.dates, date_int, side="left"))
+        if self.dates[idx] == date_int:
+            return int(self.dates[idx + shift])
+        if shift > 0:
+            idx -= 1
+        return int(self.dates[idx + shift])
+
+    def is_business_day(self, date: Union[str, int]) -> bool:
+        """
+        tells if the given date is a business day
+
+        Args:
+            date: given date
+
+        Return:
+            whether the given date is a business day or not
+        """
+        date_int = int(date)
+        return date_int in self.date_set
+
+    def get_business_days(self, start_date: Union[str, int],
+                          end_date: Union[str, int]) -> List[int]:
+        """
+        retrieve a list of business days given a date range
+
+        Args:
+            start_date: start of the range (inclusive)
+            end_date: end of the range (inclusive)
+        
+        Return:
+            list of dates (integer)
+        """
+        start: int = int(start_date)
+        end: int = int(end_date)
+        st_idx: int = np.searchsorted(self.dates, start, side="left")
+        rt_idx: int = np.searchsorted(self.dates, end, side="right")
+        return [int(x) for x in self.dates[st_idx:rt_idx]]
+#############################################################################################
 
 class SigFileType(IntEnum):
     csv = 0
     npy = 1
-
-
-class InsDataCache:
-    def __init__(self, name: str):
-        self.name: str = name
-        self.exchtime = []
-        self.localtime = []
-        self.ap = []
-        self.bp = []
-    
-    def push(self, ss: MdSnapshot):
-        self.exchtime.append(ss.exchtime)
-        self.localtime.append(ss.localtime)
-        level1: MdLevel = ss.levels[0]
-        self.ap.append(level1.ap)
-        self.bp.append(level1.bp)
-
 
 def hhmmssf_to_exchtime(val: str) -> int:
     hh: int = int(val[:2])
@@ -46,7 +114,6 @@ def hhmmssf_to_exchtime(val: str) -> int:
     if hh >= 18:
         time -= 24 * 3600 * int(1e9)
     return time
-
 
 cdef void match_sig_with_md(
         const int sig_nr,
@@ -73,6 +140,260 @@ cdef void match_sig_with_md(
 
         sig_idx += 1
 
+def localtime_shift_in_futret_bias(x:np.uint64, futret_bias: int, session: List) -> np.uint64:
+    for i in range(len(session)-1):
+        if x > session[i][1] - futret_bias and x <= session[i][1] :
+            # print("shift localtime:" + str(pd.to_datetime(x, unit='ns'))+"-->"+ str(pd.to_datetime(x + (session[i+1][0] - session[i][1]) , unit='ns')))
+            x += (session[i+1][0] - session[i][1]) 
+    return x
+
+def session_maker(date_list: List,  session: List):
+    # print("session_maker:")
+    ans = []
+    for date in date_list:
+        str_time = str(date) + '00:00:00'
+        time_stamp = time.mktime(time.strptime(str_time, '%Y%m%d%H:%M:%S'))
+        ts = time_stamp * int(1e9)
+        for s in session:
+            ans.append([np.uint64(i + ts) for i in s])
+    
+    # for s in ans:
+    #     for i in s:
+    #         print(i)
+    #         print(pd.to_datetime(i, unit='ns'))
+    # print("session_maker end")
+
+    return ans
+
+class InsDataCache:
+
+    def __init__(self, tk_exch: str):
+        self.tk_exch: str = tk_exch
+        self.exchtime = []
+        self.localtime = []
+        self.ap = []
+        self.bp = []
+    
+    def push(self, ss: MdSnapshot):
+        self.exchtime.append(ss.exchtime)
+        self.localtime.append(ss.localtime)
+        level1: MdLevel = ss.levels[0]
+        self.ap.append(level1.ap)
+        self.bp.append(level1.bp)
+
+    def cache2ret_df(self):
+        return  pd.DataFrame(
+                {
+                    "exchtime": self.exchtime,
+                    "localtime": self.localtime,
+                    "ap": self.ap,
+                    "bp": self.bp
+                })
+
+
+def calculate_ic_for_target(ret_df: pd.DataFrame, sig_df: pd.DataFrame, futret_bias: int) -> pd.DataFrame:
+    ret_df["localtime"] = ret_df["localtime"].astype(np.uint64)
+    ret_df["mid_px"] = (ret_df["ap"] + ret_df["bp"]) / 2
+    
+    sig_df["localtime"] = sig_df["localtime"].astype(np.uint64)
+    sig_df["mid_px"] = np.nan
+    sig_df["fut_localtime"] = sig_df["localtime"] + futret_bias
+    sig_df["fut_mid_px"] = np.nan
+    match_sig_with_md(len(sig_df.index),
+            sig_df["localtime"].values,
+            sig_df["mid_px"].values,
+            sig_df["fut_localtime"].values,
+            sig_df["fut_mid_px"].values,
+            len(ret_df.index),
+            ret_df["localtime"].values,
+            ret_df["mid_px"].values)
+    sig_df["fut_ret"] = (sig_df["fut_mid_px"] / sig_df["mid_px"]) - 1
+    return sig_df
+
+class MdCache(ABC):
+
+    def __init__(self, futret_bias: Dict[int, int], fout: _io.TextIOWrapper):
+        self.futret_bias: Dict[int, str] = futret_bias
+        self.fout = fout
+        self.name2sessions: Dict[string, List] = {}
+
+    @abstractmethod
+    def on_sod(self, ev: SodEvent, sig_df: pd.DataFrame, today: int, date: int):
+        pass
+
+    @abstractmethod
+    def on_snapshot(self, ev: SnapshotEvent):
+        pass
+
+    @abstractmethod
+    def on_eod(self, date: int):
+        pass
+
+    @abstractmethod
+    def __del__(self):
+        pass
+
+class DailyMdCache(MdCache):
+
+    def __init__(self, futret_bias: Dict[int, int], fout:_io.TextIOWrapper):
+        super().__init__(futret_bias, fout)
+        self.sig_df: pd.DataFrame = None
+        self.ins_cache: Dict[int, InsDataCache] = {}
+        self.fout.write(f"date," + ",".join(self.futret_bias.values()) + "\n")
+     
+    def on_sod(self, ev: SodEvent, sig_df: pd.DataFrame, today: int, date: int):
+        if today != date:
+            self.ins_cache.clear()
+            self.sig_df = sig_df
+
+        for i in range(ev.ins_nr):
+            ms_ptr: int = ctypes.cast(ev.ms[i], ctypes.c_void_p).value
+            ms: MdStatic = ev.ms[i].contents
+            print(f"\t{i+1},{ms_ptr},{ms.instrument}")            
+            tk_exch = ms.ticker.decode("utf8") + "." + ms.exchange.decode("utf8")            
+            self.ins_cache[ms_ptr] = InsDataCache(tk_exch)
+            if tk_exch not in self.name2sessions.keys():
+                print("collect sessions info")
+                sessions = []
+                for j in range(ms.session_nr):
+                    sessions.append([ms.session[j].begin, ms.session[j].end])
+                self.name2sessions[tk_exch] = sessions
+        
+    def on_snapshot(self, ev: SnapshotEvent):
+        ms_ptr: int = ctypes.cast(ev.ms, ctypes.c_void_p).value
+        # ms: MdStatic = ev.ms.contents
+        ss: MdSnapshot = ev.snapshot.contents
+        self.ins_cache[ms_ptr].push(ss)
+
+    def on_eod(self, date: int):
+        print(f"{date},calculating ic")
+        fut_ret_all = []
+        for futret_bias in self.futret_bias:
+            sigs = []
+            fut_ret = []
+            for cache in self.ins_cache.values():
+                sessions = session_maker([date], self.name2sessions[cache.tk_exch])
+                sig_df = self.sig_df[["exchtime", "localtime", cache.tk_exch]].copy()
+                # sig_df.to_csv(f"{date}_{cache.tk_exch}_sig_before.csv", sep=',')
+                sig_df['localtime'] = sig_df['localtime'].apply(localtime_shift_in_futret_bias, args = (futret_bias, sessions))
+                sig_df["localtime"] = sig_df["localtime"].astype(np.uint64)
+                # sig_df.to_csv(f"{date}_{cache.tk_exch}_sig_after.csv", sep=',')
+                ret_df = cache.cache2ret_df()
+                sig_df: pd.DataFrame = calculate_ic_for_target(ret_df, sig_df, futret_bias)
+                # sig_df["time"] = pd.to_datetime(sig_df['localtime'],unit='ns')
+                # sig_df.to_csv(f"{date}_{cache.tk_exch}_sig_latest.csv", sep=',')
+                sigs.append(sig_df[cache.tk_exch])
+                fut_ret.append(sig_df["fut_ret"])
+           
+            sig_1d = np.ma.masked_invalid(np.ravel(sigs))
+            fut_ret_1d = np.ma.masked_invalid(np.ravel(fut_ret))
+            corr = np.ma.corrcoef(sig_1d, fut_ret_1d)
+            fut_ret_all.append(corr[0][1])
+        self.fout.write(f"{date}," + ",".join(f"{x}" for x in fut_ret_all) + "\n")
+
+    def __del__(self):
+        pass
+
+class ContinuousMDCache(MdCache):
+
+    def __init__(self, futret_bias: Dict[int, int], fout: _io.TextIOWrapper):
+        super().__init__(futret_bias, fout)
+        self.ins_cache: Dict[string, InsDataCache] = {}
+        self.ins_cache_list: List = []
+        self.sig_df_list: List = []
+        self.date_list: List = []
+        self.cal : Calendar  = Calendar.get_instance()
+        self.fout.write(",".join(self.futret_bias.values()) + "\n")
+
+    def on_sod(self, ev: SodEvent, sig_df: pd.DataFrame, today: int, date: int):
+        # For a new day, save the previous day's data 
+        if today != date:
+            # print("today:"+ str(today)+ " date" + str(date))
+            if today: 
+                self.ins_cache_list.append(self.ins_cache)
+                self.ins_cache.clear()
+            self.sig_df_list.append(sig_df)
+            self.date_list.append(date)
+
+        for i in range(ev.ins_nr):
+            ms: MdStatic = ev.ms[i].contents
+            print(f"\t{i+1}, {ms.instrument}")
+            tk_exch = ms.ticker.decode("utf8") + "." + ms.exchange.decode("utf8")
+            self.ins_cache[tk_exch] = InsDataCache(tk_exch)
+            if tk_exch not in self.name2sessions.keys():
+                print("collect sessions info")
+                sessions = []
+                for j in range(ms.session_nr):
+                    sessions.append([ms.session[j].begin, ms.session[j].end])
+                self.name2sessions[tk_exch] = sessions
+
+    def on_snapshot(self, ev: SnapshotEvent):
+        ms: MdStatic = ev.ms.contents
+        ss: MdSnapshot = ev.snapshot.contents
+        self.ins_cache[ms.ticker.decode("utf8") + "." + ms.exchange.decode("utf8")].push(ss)
+
+    def on_eod(self, date: int):
+        if date == 20230710:
+            self.__del__()
+            
+    def __del__(self):
+        self.ins_cache_list.append(self.ins_cache)
+        print(f"calculating ic")
+        sig_ret_date = []
+
+        Continuous_sig :pd.DataFrame = self.sig_df_list[0]
+        Continuous_ret :Dict[string, pd.DataFrame] = {}
+        for tk_exch, cache in self.ins_cache_list[0].items():
+            Continuous_ret[tk_exch] = cache.cache2ret_df()
+        
+        date_list = [self.date_list[0]]
+
+        for i in range(len(self.date_list)-1):
+            if self.date_list[i+1] == self.cal.next_business_day(self.date_list[i]):
+                Continuous_sig = pd.concat([Continuous_sig, self.sig_df_list[i+1]])
+                for cache in self.ins_cache_list[i+1].values():
+                    Continuous_ret[cache.tk_exch] = pd.concat([Continuous_ret[cache.tk_exch], cache.cache2ret_df()])
+                date_list.append(self.date_list[i+1])
+            else:
+                sig_ret_date.append({
+                  "date": date_list,
+                  "sig_df": Continuous_sig,
+                  "ret_df": Continuous_ret
+                })
+                Continuous_sig = self.sig_df_list[i+1]
+                for cache in self.ins_cache_list[i+1].values():
+                    Continuous_ret[cache.tk_exch] = cache.cache2ret_df()
+                date_list = [self.date_list[i+1]]
+
+        sig_ret_date.append({
+            "date": date_list,
+            "sig_df": Continuous_sig,
+            "ret_df": Continuous_ret
+        })
+        # print(sig_ret_date)
+
+        fut_ret_all = []
+        for futret_bias in self.futret_bias:
+            sigs = []
+            fut_ret = []
+            for srd in sig_ret_date:
+                total_sig_df = srd['sig_df']
+                total_ret_df =  srd['ret_df']
+                total_date = srd['date']
+                for tk_exch in total_ret_df.keys():
+                    sessions = session_maker(total_date, self.name2sessions[tk_exch])
+                    sig_df = total_sig_df[["exchtime", "localtime", tk_exch]].copy()
+                    sig_df['localtime'] = sig_df['localtime'].apply(localtime_shift_in_futret_bias, args = (futret_bias, sessions))
+                    sig_df["localtime"] = sig_df["localtime"].astype(np.uint64)
+                    sig_df: pd.DataFrame = calculate_ic_for_target(total_ret_df[tk_exch], sig_df, futret_bias)
+                    # sig_df.to_csv(f"{cache.tk_exch}_sig.csv", sep=',')
+                    sigs.append(sig_df[tk_exch])
+                    fut_ret.append(sig_df["fut_ret"])
+            sig_1d = np.ma.masked_invalid(np.ravel(sigs))
+            fut_ret_1d = np.ma.masked_invalid(np.ravel(fut_ret))
+            corr = np.ma.corrcoef(sig_1d, fut_ret_1d)
+            fut_ret_all.append(corr[0][1])
+        self.fout.write(",".join(f"{x}" for x in fut_ret_all) + "\n")
 
 class ICCalculator(SignalBase):
 
@@ -85,7 +406,6 @@ class ICCalculator(SignalBase):
         self.today: int = 0
         self.sig_df: pd.DataFrame = None
         self.futret_bias: Dict[int, str] = {}
-        self.ins_cache: Dict[int, InsDataCache] = {}
 
     def load_signal(self):
 
@@ -139,83 +459,23 @@ class ICCalculator(SignalBase):
         odir.mkdir(parents=True, exist_ok=True)
         ofile: Path = odir / f"{self.signame}.csv"
         self.fout = open(ofile, "w", buffering=1)
-        self.fout.write(f"date," + ",".join(self.futret_bias.values()) + "\n")
+        cache_type: str = str(cfg["cache_type"])
+        self.cache = DailyMdCache(self.futret_bias, self.fout) if cache_type == "daily" else ContinuousMDCache(self.futret_bias, self.fout) 
 
     def on_sod(self, date: int, ev: SodEvent):
         print(f"on_sod:{date},ins_nr:{ev.ins_nr}")
+        today = self.today
         if self.today != date:
             self.today = date
             self.load_signal()
-            self.ins_cache.clear()
 
-        for i in range(ev.ins_nr):
-            ms_ptr: int = ctypes.cast(ev.ms[i], ctypes.c_void_p).value
-            ms: MdStatic = ev.ms[i].contents
-            print(f"\t{i+1},{ms_ptr},{ms.instrument}")
-            self.ins_cache[ms_ptr] = InsDataCache(ms.instrument.decode("utf8"))
+        self.cache.on_sod(ev, self.sig_df, today, date)
 
     def on_eod(self, date: int):
-        def __calculate_ic_for_target(target: str, futret_bias: int) -> pd.DataFrame:
-            sig_df = self.sig_df[["exchtime", "localtime", target]]
-            for ms_ptr, cache in self.ins_cache.items():
-                ms: MdStatic = MdStatic.from_address(ms_ptr)
-                ms_tgt: str = ms.ticker.decode("utf8") + "." + ms.exchange.decode("utf8")
-                if ms_tgt == target:
-                    break
-            else:
-                raise RuntimeError(f"no data found for {target}")
-            ret_df = pd.DataFrame(
-                {
-                    "exchtime": cache.exchtime,
-                    "localtime": cache.localtime,
-                    "ap": cache.ap,
-                    "bp": cache.bp
-                })
-            ret_df["exchtime"] = ret_df["exchtime"].astype(np.int64)
-            ret_df["localtime"] = ret_df["localtime"].astype(np.uint64)
-            ret_df["mid_px"] = (ret_df["ap"] + ret_df["bp"]) / 2
-
-            sig_df["mid_px"] = np.nan
-            sig_df["fut_localtime"] = sig_df["localtime"] + futret_bias
-            sig_df["fut_mid_px"] = np.nan
-            match_sig_with_md(len(sig_df.index),
-                    sig_df["localtime"].values,
-                    sig_df["mid_px"].values,
-                    sig_df["fut_localtime"].values,
-                    sig_df["fut_mid_px"].values,
-                    len(ret_df.index),
-                    ret_df["localtime"].values,
-                    ret_df["mid_px"].values)
-            sig_df["fut_ret"] = (sig_df["fut_mid_px"] / sig_df["mid_px"]) - 1
-            return sig_df
-
-        print(f"{date},calculating ic")
-        targets = [x for x in self.sig_df.columns if x not in ["exchtime", "localtime"]]
-
-        fut_ret_all = []
-        for futret_bias in self.futret_bias:
-            sigs = []
-            fut_ret = []
-            for tgt in targets:
-                sig_df: pd.DataFrame = __calculate_ic_for_target(tgt, futret_bias)
-                sigs.append(sig_df[tgt])
-                fut_ret.append(sig_df["fut_ret"])
-            sig_1d = np.ma.masked_invalid(np.ravel(sigs))
-            fut_ret_1d = np.ma.masked_invalid(np.ravel(fut_ret))
-            corr = np.ma.corrcoef(sig_1d, fut_ret_1d)
-            fut_ret_all.append(corr[0][1])
-        self.fout.write(f"{date}," + ",".join(f"{x}" for x in fut_ret_all) + "\n")
-
+        self.cache.on_eod(date)
 
     def on_snapshot(self, ev: SnapshotEvent):
-        ms_ptr: int = ctypes.cast(ev.ms, ctypes.c_void_p).value
-        # ms: MdStatic = ev.ms.contents
-        ss: MdSnapshot = ev.snapshot.contents
-        self.ins_cache[ms_ptr].push(ss)
-
-    def on_cs_snapshot(self, ev: CsSnapshotEvent):
-        pass
-
+        self.cache.on_snapshot(ev)
 
 def pysig_create():
     return ICCalculator()
